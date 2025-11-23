bb8648400abd3b30f9ae88e30cb58084f23fce96
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-11-23 13:57:18 -0500

    Made so React Router is used for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { HomePage } from './pages/HomePage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { HomePage } from "./pages/HomePage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [{ path: "/", element: <HomePage /> }],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HomePage />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
