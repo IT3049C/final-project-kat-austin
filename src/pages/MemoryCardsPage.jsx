521c76b0830c7e9ebcba4686cfafc6110e90aacb
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-04 19:33:55 -0500

    Added starter implementation of cards in a grid formation

--- src/pages/MemoryCardsPage.jsx
+++ src/pages/MemoryCardsPage.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import { MemoryCard } from "../components/memory-cards/MemoryCard";
+
+const COLUMNS = 6;
+const ROWS = 3;
+
 export function MemoryCardsPage() {
+  const [grid] = useState(setupGrid);
+
   return (
     <>
       <header>
         <h2>Memory Cards</h2>
       </header>
-      <main></main>
+      <main>
+        <div
+          id="memory-card-container"
+          style={{ gridTemplateColumns: `repeat(${COLUMNS}, auto)` }}
+        >
+          {grid.map((v) => (
+            <MemoryCard {...v} revealed={true} />
+          ))}
+        </div>
+      </main>
     </>
   );
+}
+
+function setupGrid() {
+  const grid = [];
+
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLUMNS; col++) {
+      grid.push({ icon: "😀" });
+    }
+  }
+  return grid;
 }
