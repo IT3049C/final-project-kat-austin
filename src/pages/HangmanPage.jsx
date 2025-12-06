98650c498d32d7f763bbd3c20ed0cafee0cba331
SpiteTheGhost <ghostmori15@gmail.com>
2025-12-05 20:12:08 -0500

    hangman page

---
+++ src/pages/HangmanPage.jsx
@@ -0,0 +1,9 @@
+import HangmanGame from '../components/hangman/HangmanGame.jsx';
+
+export default function HangmanPage() {
+  return (
+    <div className="hangman-page">
+      <HangmanGame />
+    </div>
+  );
+}
