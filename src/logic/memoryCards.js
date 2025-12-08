a44a841406e635d588ac683ddbec85596fb0c5a1
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-08 14:03:18 -0500

    Added some possible emoji icons to use for the memory cards

--- src/logic/memoryCards.js
+++ src/logic/memoryCards.js
@@ -2,3 +2,19 @@ export const config = Object.freeze({
   columns: 6,
   rows: 3
 });
+
+export const emojis = Object.freeze([
+  "🎈",
+  "🎃",
+  "🍒",
+  "🍑",
+  "🍆",
+  "🍕",
+  "🎲",
+  "🔔",
+  "💣",
+  // "🌻",
+  // "🍋",
+  // "🍊",
+  // "🧸",
+]);
