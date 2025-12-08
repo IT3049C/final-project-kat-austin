36ccebe3339c6c66eaf4df4889eb0c36c3f34462
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-08 14:07:11 -0500

    Made memory cards stay up if a match is found, and flip back over if not

--- src/pages/MemoryCardsPage.jsx
+++ src/pages/MemoryCardsPage.jsx
@@ -1,16 +1,64 @@
 import { useState } from "react";
 import { MemoryCard } from "../components/memory-cards/MemoryCard";
-import { config } from "../logic/memoryCards"
+import { config, emojis } from "../logic/memoryCards";
+
+const SELECTED_CARD_LIMIT = 2;
 
 export function MemoryCardsPage() {
   const [grid, setGrid] = useState(setUpGrid());
+  // Tracks the keys of the cards that have been selected. Should only contain
+  // two at a time.
+  /** @type {[number[], React.Dispatch<React.SetStateAction<number[]>>]} */
+  const [selectedCardKeys, setSelectedCardKeys] = useState([]);
 
-  function handleCardClick(key) {
-    setGrid(
-      grid.map((card, i) =>
-        i === key ? { ...card, revealed: !card.revealed } : card
-      )
-    );
+  function handleCardClick(clickedCard) {
+    if (
+      selectedCardKeys.length < SELECTED_CARD_LIMIT &&
+      !clickedCard.revealed &&
+      !clickedCard.matchFound
+    ) {
+      setGrid(
+        grid.map((card) =>
+          card.key === clickedCard.key ? { ...card, revealed: true } : card
+        )
+      );
+      setSelectedCardKeys([...selectedCardKeys, clickedCard.key]);
+    }
+  }
+
+  /** 
+   * Checks if the two most recently selected cards match 
+   */
+  function checkCards() {
+    if (selectedCardKeys.length === SELECTED_CARD_LIMIT) {
+      const selectedCard1 = grid.find(
+        (card) => card.key === selectedCardKeys[0]
+      );
+      const selectedCard2 = grid.find(
+        (card) => card.key === selectedCardKeys[1]
+      );
+      let cardCheckResult;
+
+      if (selectedCard1.icon === selectedCard2.icon) {
+        // Indicate that a match has been found for that card
+        cardCheckResult = { matchFound: true };
+      } else {
+        // Flip the cards back over if they don't match
+        cardCheckResult = { revealed: false };
+      }
+      setGrid(
+        grid.map((card) =>
+          card.key === selectedCard1.key || card.key === selectedCard2.key
+            ? { ...card, ...cardCheckResult }
+            : card
+        )
+      );
+      setSelectedCardKeys([]);
+    }
+  }
+
+  if (selectedCardKeys.length === SELECTED_CARD_LIMIT) {
+    setTimeout(checkCards, 2000);
   }
 
   return (
@@ -27,7 +75,7 @@ export function MemoryCardsPage() {
             <MemoryCard
               {...card}
               key={card.key}
-              onClick={() => handleCardClick(card.key)}
+              onClick={() => handleCardClick(card)}
             />
           ))}
         </div>
@@ -41,7 +89,12 @@ function setUpGrid() {
 
   for (let row = 0; row < config.rows; row++) {
     for (let col = 0; col < config.columns; col++) {
-      grid.push({ key: grid.length, icon: "😀", revealed: false });
+      grid.push({
+        key: grid.length,
+        icon: randomElement(emojis),
+        revealed: false,
+        matchFound: false,
+      });
     }
   }
   return grid;
