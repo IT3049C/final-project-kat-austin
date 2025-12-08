bd7c41eef2590476d043a66b023f7b94a9a9947a
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-08 14:11:51 -0500

    Changed memory cards into button elements

--- src/components/memory-cards/MemoryCard.jsx
+++ src/components/memory-cards/MemoryCard.jsx
@@ -2,12 +2,15 @@
  * @param {object} props
  * @param {string} props.icon
  * @param {boolean} props.revealed
- * @param {MouseEventHandler<HTMLDivElement>} props.onClick
+ * @param {MouseEventHandler<HTMLButtonElement>} props.onClick
  */
-export function MemoryCard({ icon, onClick, revealed = false}) {
+export function MemoryCard({ icon, onClick, revealed = false }) {
   return (
-    <div className={revealed ? "memory-card" : "memory-card-hidden"} onClick={onClick}>
+    <button
+      className={revealed ? "memory-card" : "memory-card-hidden"}
+      onClick={onClick}
+    >
       {revealed ? <p>{icon}</p> : <p></p>}
-    </div>
+    </button>
   );
 }
