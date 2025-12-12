2a0b034c0402304cf8df1cad1e427309e1f8c022
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-12 15:06:23 -0500

    Made tic-tac-toe multiplayer

--- src/pages/TicTacToePage.jsx
+++ src/pages/TicTacToePage.jsx
@@ -2,55 +2,106 @@ import { useState } from "react";
 import { loadSettings } from "../logic/storage";
 import { GameHeader } from "../components/GameHeader";
 import { Board } from "../components/tic-tac-toe/Board";
+import { useGameRoom } from "../hooks/useGameRoom";
+import { calculateWinner } from "../logic/ticTacToe";
+import { Multiplayer } from "../components/Multiplayer";
+
+/**
+ * @typedef {object} TicTacToeState
+ * @property {string[]} players
+ * @property {number} turn The number of turns that have elapsed
+ * @property {(string | null)[]} board
+ * @property {string | null} winner
+ * @property {number} version
+ * @property {string | null} updatedBy
+ * @property {number | null} updatedAt
+ */
+
+/** @type {TicTacToeState} */
+const DEFAULT = {
+  players: [],
+  turn: 0,
+  board: Array(9).fill(null),
+  winner: null,
+  version: 0,
+  updatedBy: null,
+  updatedAt: null,
+};
 
 export function TicTacToePage() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  // Tracks who joined the room vs created the room
+  const [joinedRoom, setJoinedRoom] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
+  const { roomId, state, isLoading, error, setRoomId, createRoom, pushState } =
+    useGameRoom({
+      refetchInterval: 1000,
+    });
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = "Go to move #" + move;
-    } else {
-      description = "Go to game start";
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
+  const xIsNext = state?.turn % 2 === 0;
   const settings = loadSettings();
   const playerName = settings?.name || "Player";
 
+  /**
+   * @param {("X" | "O" | null)[]} nextSquares
+   */
+  function handlePlay(nextSquares) {
+    const turnOf = xIsNext ? "O" : "X";
+    if (turnOf === "X" && !joinedRoom) return;
+    if (turnOf === "O" && joinedRoom) return;
+
+    /** @type {TicTacToeState} */
+    const gameState = {
+      players: state.players,
+      turn: state.turn + 1,
+      board: nextSquares,
+      winner: calculateWinner(nextSquares),
+      version: state.version + 1,
+      updatedBy: xIsNext ? "O" : "X",
+      updatedAt: Date.now(),
+    };
+    pushState(gameState);
+  }
+
+  if (isLoading) {
+    if (!loading) setLoading(true);
+    return <p>Loading room...</p>;
+  } else if (error) {
+    if (loading) setLoading(false);
+    console.error(error);
+    return <p>{error.message}</p>;
+  }
+
+  if (loading) {
+    console.log(joinedRoom);
+    setJoinedRoom(true);
+    setLoading(false);
+    pushState({ ...state, players: [...state.players, "O"] });
+  }
+
   return (
     <>
-      <GameHeader gameName="Tic-Tac-Toe" playerName={playerName}/>
-      <div className="tic-tac-toe-game">
-        <div className="game-board">
-          <Board
-            xIsNext={xIsNext}
-            squares={currentSquares}
-            onPlay={handlePlay}
-          />
-        </div>
-        <div className="tic-tac-toe-info">
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      <GameHeader gameName="Tic-Tac-Toe" playerName={playerName} />
+      <Multiplayer
+        onCreateRoom={createRoom}
+        initialState={{ ...DEFAULT, players: ["X"] }}
+        roomId={roomId}
+        setRoomId={setRoomId}
+      />
+      {roomId && (
+        <>
+          <p>You are {joinedRoom ? "O" : "X"}</p>
+          <div className="tic-tac-toe-game">
+            <div className="game-board">
+              <Board
+                xIsNext={xIsNext}
+                squares={state?.board ?? []}
+                onPlay={handlePlay}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
