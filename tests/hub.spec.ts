3034ed5c0b693ae951f46b18efae4084bccd2889
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-13 17:58:36 -0500

    Finished tests for the hub

--- tests/hub.spec.ts
+++ tests/hub.spec.ts
@@ -7,7 +7,12 @@ test.describe("Game Hub", () => {
     /* Load landing page */
     await page.goto("/");
 
-    await page.getByRole("heading", { name: "GameHub" }).click();
+    await expect(
+      page.getByRole("heading", { name: "GameHub", exact: true })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Welcome to GameHub" })
+    ).toBeVisible();
 
     /* Verify game list present */
     await expect(
@@ -20,27 +25,38 @@ test.describe("Game Hub", () => {
       page.getByRole("link", { name: "Memory Cards" })
     ).toBeVisible();
 
+    /* Capture player name */
+    await page.getByRole("textbox", { name: "Your Name:" }).fill(PLAYER_NAME);
+    await page.getByRole("button", { name: "Save Name" }).click();
+    await expect(page.getByText(`Current name: ${PLAYER_NAME}`)).toBeVisible();
+    await expect(page.getByRole("strong")).toContainText(PLAYER_NAME);
+
     /* Navigate to every game page and back, along with verifying player name present */
+    // Rock Paper Scissors
     await page.getByRole("link", { name: "Rock Paper Scissors" }).click();
     await expect(
       page.getByRole("heading", { name: "Rock Paper Scissors" })
     ).toBeVisible();
     await expect(page.getByTestId("greeting")).toContainText(PLAYER_NAME);
 
+    // Tic-tac-toe
     await page.getByRole("link", { name: "Tic-Tac-Toe" }).click();
     await expect(
       page.getByRole("heading", { name: "Tic-Tac-Toe" })
     ).toBeVisible();
     await expect(page.getByTestId("greeting")).toContainText(PLAYER_NAME);
 
+    // Wordle
     await page.getByRole("link", { name: "Wordle" }).click();
     await expect(page.getByRole("heading", { name: "Wordle" })).toBeVisible();
     await expect(page.getByTestId("greeting")).toContainText(PLAYER_NAME);
 
+    // Hangman
     await page.getByRole("link", { name: "Hangman" }).click();
     await expect(page.getByRole("heading", { name: "Hangman" })).toBeVisible();
     await expect(page.getByTestId("greeting")).toContainText(PLAYER_NAME);
 
+    // Memory Cards
     await page.getByRole("link", { name: "Memory Cards" }).click();
     await expect(
       page.getByRole("heading", { name: "Memory Cards" })
