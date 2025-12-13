2608c2f093569671da8c1f623bec7beb45522258
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-13 15:55:12 -0500

    Moved useGameRoom helper methods into own file

--- src/hooks/useGameRoom.js
+++ src/hooks/useGameRoom.js
@@ -2,49 +2,7 @@
 // https://uc.instructure.com/courses/1807548/pages/simulating-multiplayer-games-with-the-games-room-api
 import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-
-const BASE = "https://game-room-api.fly.dev";
-
-/**
- * Creates a new room on the server.
- * @returns {Promise<{ roomId: string; gameState: any }} The newly created room
- * and its state.
- */
-async function apiCreateRoom(initialState) {
-  const res = await fetch(`${BASE}/api/rooms`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ initialState }),
-  });
-  if (!res.ok) throw new Error("Failed to create room");
-  return res.json();
-}
-
-/**
- * Reads an existing room on the server.
- * @param {string} roomId the id of the room you want to read.
- * @returns {Promise<{ id: string; gameState: any }>}
- */
-async function apiGetRoom(roomId) {
-  const res = await fetch(`${BASE}/api/rooms/${roomId}`);
-  if (!res.ok) throw new Error("Room not found");
-  return res.json();
-}
-
-/**
- * Updates an existing room on the server.
- * @param {string} roomId The id of the room you want to update.
- * @returns {Promise<{ id: string; gameState: any }>} The updated room object.
- */
-async function apiUpdateRoom(roomId, gameState) {
-  const res = await fetch(`${BASE}/api/rooms/${roomId}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ gameState }),
-  });
-  if (!res.ok) throw new Error("Failed to update room");
-  return res.json();
-}
+import { apiCreateRoom, apiGetRoom, apiUpdateRoom } from "../utils/gameRoom";
 
 /**
  * @param {object} options
